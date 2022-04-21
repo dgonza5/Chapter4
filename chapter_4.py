6d2303c22c8d77923a37c90bec33d4f732cacf10
Deeno Gonzalez <deenogonzalez@DeeLuisaas-MBP.attlocal.net>
2022-04-20 22:11:56 -0700

    working on section 4.1

---
+++ chapter_4.py
@@ -0,0 +1,62 @@
+
+
+
+# def get_number():
+#     return 5
+
+# def set_number(num):
+#     number = num
+#     print("From set_number, num is: ", str(num))
+#     return number
+
+
+# num = get_number()
+# set_number(6)
+
+# print("End of Program, Num is: ", str(num))
+
+
+# def add_number(num1, num2):
+#     print("num1: ", str(num1))
+#     print("num2: ", str(num2))
+#     val = num1 + num2
+#     print("from add_number, val is: ", str(val))
+#     return val
+
+
+# output = add_number(2, 5) # 7
+
+# print("Output of {} and {} is {}.".format(str(2), str(5), output))
+
+
+
+# def get_firstname(full_name): # Dionicio Gonzalez
+#     space = full_name.index(" ")
+#     first_name = full_name[:space]
+#     return first_name
+
+# # my_name = get_firstname("Dionicio Gonzalez")
+# # print("Hello, my name is " + my_name + ".")
+
+
+# names = [
+#     "Dionicio Gonzalez", 
+#     "Joe Biden", 
+#     "Mike Tyson", 
+#     "Tom Brady", 
+#     "Kobe Bryant"
+# ]
+
+# for name in names:
+#     # print("Name: ", name)
+#     my_name = get_firstname(name)
+#     # print("Hello, my name is " + my_name + ".")
+#     # print(my_name == "Tom")
+#     if my_name == "Tom":
+#         print(my_name + " plays football!")
+#     elif my_name == "Kobe":
+#         print(my_name + " plays basketball!")
+#     elif my_name == "Mike":
+#         print(my_name + " is into boxing!")
+#     else:
+#         print(my_name + " is not not into sports!")
